c4b71094c90e9b443d67f4f19a507bcedc06cb59
swetshaw <swt.shw94@gmail.com>
2021-03-31 01:29:55 +0530

    build added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static("build"));
+
 morgan.token("data", (request) => {
   if (request.method == "POST") return " " + JSON.stringify(request.body);
   else return " ";
